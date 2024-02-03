44fd56390c4606397619b91901b3ad39eebb3617
Naveen Akash <naveen.akash0904@gmail.com>
2024-02-03 22:21:23 +0530

    Solved events alignment error

--- frontend/src/page/Events.tsx
+++ frontend/src/page/Events.tsx
@@ -1,25 +1,23 @@
-import styles from "../styles/page/events.module.css";
+import styles from "../styles/page/eventInfo.module.css";
 import eve from "../assets/Ticket_img_svg_file_page-0001-removebg-preview.png";
-import { useLoaderData, useParams, useRouteLoaderData } from "react-router";
+// import { useParams } from "react-router";
 
 const Events: React.FC = () => {
-  const { department } = useParams();
-
+  // const { department } = useParams();
   const eventDetail: {
     id: number;
     name: string;
     km: string;
   }[] = [
-    { id: 1, name: "Event - 1 Name", km: "#know" },
-    { id: 2, name: "Event - 2 Name", km: "#know" },
-    { id: 3, name: "Event - 3 Name", km: "#know" },
-    { id: 4, name: "Event - 4 Name", km: "#know" },
-  ];
+      { id: 1, name: "Event - 1 Name", km: "#know" },
+      { id: 2, name: "Event - 2 Name", km: "#know" },
+      { id: 3, name: "Event - 3 Name", km: "#know" },
+      { id: 4, name: "Event - 4 Name", km: "#know" },
+    ];
 
   return (
     <div className={styles.body}>
       <div className={styles.container}>
-        <a href="#Events">&larr; Back To Events</a>
         <div className={styles.list}>
           {eventDetail.map((data) => (
             <div key={data.id} className={styles.cardContainer}>
