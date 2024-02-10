ad07a40d6907ebb2b74aec6b70458373f043a839
Naveen Akash <naveen.akash0904@gmail.com>
2024-02-10 16:57:45 +0530

    Added footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,100 +8,66 @@ import utube from "../assets/images/icons/utube.png";
 import emailIcon from "../assets/images/icons/Email.png";
 import locationIcon from "../assets/images/icons/Spacer Icon Wrapper.png";
 import callIcon from "../assets/images/icons/Phone.png";
+import {
+  IoLogoFacebook,
+  IoLogoInstagram,
+  IoMail,
+  IoCall,
+  IoLocation,
+} from "react-icons/io5";
 
-const Footer: React.FC = () => {
-  const media: { [key: string]: string }[] = [
-    { key: "fb", value: fb },
-    { key: "insta", value: insta },
-    { key: "twitter", value: twitter },
-    { key: "linkedIn", value: linkedIn },
-    { key: "utube", value: utube },
-  ];
-  const menu: { key: string }[] = [
-    { key: "Home" },
-    { key: "About" },
-    { key: "Events" },
-    { key: "Get Passes" },
-    { key: "Workshops" },
-    { key: "Proshows" },
-    { key: "Location" },
-    { key: "Login" },
-  ];
+const Footer = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.svceContainer}>
-          <div className={styles.svce}>
-            <img src={svce} alt="SVCE" className={styles.img} width={"50%"} />
-            <h3>A National Level Technical Fest</h3>
-            {media.map((data) => (
-              <img
-                className={styles.mediaimage}
-                src={data.value}
-                alt={data.key}
-                style={
-                  data.key === "fb"
-                    ? { height: "40px" }
-                    : data.key === "utube"
-                    ? { width: "7%", height: "43%" , marginLeft: "5%" }
-                    : { width: "5%", height: "43%", marginLeft: "5%" }
-                }
-              />
-            ))}
-          </div>
-        </div>
-        <div className={styles.menu}>
-          <h2>Menu</h2>
-          {menu.map((data, index) => (
-            <h4 style={{ marginTop: "1%" }} key={index}>
-              {data.key}
-            </h4>
-          ))}
-        </div>
-        <div className={styles.contact}>
-          <h2>Contact Us</h2>
-          <div className={styles.connector}>
-            <img
-              src={emailIcon}
-              alt="~"
-              width={"5%"}
-              style={{ marginRight: "2%" }}
-            />
-            <p>Mail:xyz@svce.ac.in</p>
-          </div>
-          <div className={styles.connector}>
-            <img
-              src={callIcon}
-              alt="~"
-              width={"5%"}
-              style={{ marginRight: "2%" }}
-            />
-            <p>Number:9999999999</p>
-          </div>
-          <div>
-            <div className={styles.connector}>
-              <p>
-                <img
-                  src={locationIcon}
-                  alt="~"
-                  width={"12%"}
-                  style={{ marginRight: "5%" }}
-                />
-                Address:
-                <br />
-              </p>
-            </div>
-            <p>
-              Sri Venkateswara College Of Engineering Post Bag No,1,Pennalur
-              Village,Chennai-Bengaluru High Road Sriperumbudur Tk,Tamil Nadu
-              602 117
-            </p>
-          </div>
-        </div>
+    <div className={styles.footer}>
+      <div className={styles.svceContainer}>
+        <img src={svce} alt="SVCE" className={styles.img} />
+        <h2>A National Level Technical Fest</h2>
+        <IoCall className={styles.footerLogo} />
+        <IoLocation className={styles.footerLogo} />
+        <IoLogoFacebook className={styles.footerLogo} />
+        <IoLogoInstagram className={styles.footerLogo} />
+        <IoMail className={styles.footerLogo} />
       </div>
-      <p className={styles.copyrights}>
-        Copyright &copy; 2024 SVCE All Rights Reserved
-      </p>
+      <div className={styles.menuContactsDiv}>
+        <section className={styles.menu}>
+          <h2>Menu</h2>
+          <br />
+          <p>Home</p>
+          <br />
+          <p>About</p>
+          <br />
+          <p>Events</p>
+          <br />
+          <p>Get Passes</p>
+          <br />
+          <p>Workshops</p>
+          <br />
+          <p>Proshows</p>
+          <br />
+          <p>Location</p>
+        </section>
+        <section className={styles.contacts}>
+          <h2>Contact us</h2>
+          <br />
+          <p>
+            <IoMail className={styles.contactLogo} />
+            Mail: xyz.svce.ac.in
+          </p>
+          <br />
+          <p>
+            <IoCall className={styles.contactLogo} />
+            Number: 99999 99999
+          </p>
+          <br />
+          <p className={styles.addressPara}>
+            <IoLocation className={styles.contactLogo} />
+            Address: Sri Venkateswara College of Enginnering, Post Bag No.1,
+            Pennalur Village Chennai - Bangaluru High Road Sriperumbudur Tk,
+            Tamil Nadu 602117
+          </p>
+          <br />
+        </section>
+      </div>
     </div>
   );
 };
