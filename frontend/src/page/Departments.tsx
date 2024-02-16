40a467e423293f9f81f746016da5ae4a863f8571
Bhaski47 <bhaskarg2003@gmail.com>
2024-02-16 18:35:56 +0530

    Added Styles

--- frontend/src/page/Departments.tsx
+++ frontend/src/page/Departments.tsx
@@ -2,16 +2,19 @@ import styles from "../styles/page/departments.module.css";
 import Header from "../components/Header";
 import DepartmentItem from "../components/DepartmentItem";
 import departmentsData from "../data/departmentsData";
+import { useState } from "react";
 
 const DepartmentsPage: React.FC = () => {
+  const [bg,setBg] = useState('');
   return (
     <>
       <Header />
-      <div className={styles.event} id="departments">
-        <h1 className={styles.heading}>Departments</h1>
+      <div className={styles.event} id="departments" style={{backgroundImage:`url(${bg})`}}>
+        <h1 className={ !bg ? styles.heading  : ` ${styles.whiteHeader}` }>Departments</h1>
         <div className={styles.departmentGrid}>
           {departmentsData.map((ele) => (
             <DepartmentItem
+              setBG = {setBg}
               image={ele.img}
               name={ele.name}
               key={ele.id}
