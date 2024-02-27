b04dcf945fe5dc135d75276ffb357a237382d793
Naveen Akash <naveen.akash0904@gmail.com>
2024-02-27 14:50:15 +0530

    sljfviufn

--- frontend/src/page/EventForm.tsx
+++ frontend/src/page/EventForm.tsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import styles from "../styles/page/eventForm.module.css";
 
 const EventForm = () => {
   return (
     <>
       <Header />
+      <div className={styles.container}>
+        <div></div>
+      </div>
     </>
   );
 };
