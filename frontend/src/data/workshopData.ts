eb221ea416616a503490571058d4ae754dc5ec3e
Naveen Akash <naveen.akash0904@gmail.com>
2024-03-06 01:17:11 +0530

    Replaced zip

--- frontend/src/data/workshopData.ts
+++ frontend/src/data/workshopData.ts
@@ -7,11 +7,11 @@ const workshopData: {
   desc: String[];
   venue: String;
   req: String[];
-  day:String;
+  day: String;
   maxPeople: Number | null;
-  rules:String[] | null;
-  coord:String[];
-  ph:String | null
+  rules: String[] | null;
+  coord: String[];
+  ph: String | null;
 }[] = [
   {
     id: 0,
@@ -29,15 +29,15 @@ const workshopData: {
       "Engage in interactive sessions and guided exercises.",
       "Develop your own embedded projects under expert guidance.",
       "Stay updated with the latest trends and advancements in Embedded Systems Technology.",
-      "Network with fellow enthusiasts and industry professionals."
+      "Network with fellow enthusiasts and industry professionals.",
     ],
     venue: "Embedded Systems Laboratory",
     req: ["Nill"],
     maxPeople: null,
-    day:"16th March",
-    rules:[""],
-    coord:["V Kiran Yadav"],
-    ph:"+91 73391 20852"
+    day: "16th March",
+    rules: [""],
+    coord: ["V Kiran Yadav"],
+    ph: "+91 73391 20852",
   },
   {
     id: 1,
@@ -56,10 +56,10 @@ const workshopData: {
       "Need to be an automotive enthusiast or a person pursuing automotive related career",
     ],
     maxPeople: 30,
-    day:"15th March",
-    rules:[""],
-    coord:[""],
-    ph:null
+    day: "15th March",
+    rules: [""],
+    coord: [""],
+    ph: null,
   },
   {
     id: 2,
@@ -74,18 +74,18 @@ const workshopData: {
     venue: "Block 5 / Library seminar hall",
     req: ["laptops with dwsim installed"],
     maxPeople: 100,
-    day:"16th March",
-    rules:[""],
-    coord:[""],
-    ph:null
+    day: "16th March",
+    rules: [""],
+    coord: [""],
+    ph: null,
   },
   {
     id: 3,
     dept: "Association of Computer Engineers (ACE)",
     name: "A Mixed Reality AR & VR Experience",
-    duration: "9am - 2pm",
+    duration: "9AM - 2PM",
     shrtDesc:
-      "🎮 *AR & VR Workshop with Unreal Engine* Experience hands-on learning, build practical skills, and network with industry professionals in this immersive exploration of AR and VR technology. Perfect for students, professionals, and enthusiasts eager to dive into the future of digital experiences.",
+      "🎮 AR & VR Workshop with Unreal Engine Experience hands-on learning, build practical skills, and network with industry professionals in this immersive exploration of AR and VR technology. Perfect for students, professionals, and enthusiasts eager to dive into the future of digital experiences.",
     desc: [
       "🎮 *Unleash Your Creativity: AR & VR Workshop with Unreal Engine*",
       " Dive into the world of Augmented Reality (AR) and Virtual Reality (VR) in our dynamic workshop led by a certified Unreal Engine instructor.",
@@ -103,10 +103,34 @@ const workshopData: {
       "3.Kindly bring laptop.",
     ],
     maxPeople: null,
-    day:"15th March",
-    rules:["Respect copyright and intellectual property rights when using workshop materials or resources.","Any illegal or unethical behaviour "],
-    coord:["Manoj"],
-    ph:"+91 82209 69058"
+    day: "15th March",
+    rules: [
+      "Respect copyright and intellectual property rights when using workshop materials or resources.",
+      "Any illegal or unethical behaviour ",
+    ],
+    coord: ["Manoj"],
+    ph: "+91 82209 69058",
+  },
+  {
+    id: 4,
+    dept: "Marine Engineering",
+    name: "Robotic wire arc additive manufacturing",
+    duration: "1:30PM - 2:30PM",
+    shrtDesc:
+      "The aim of introducing concepts and techniques of Robotic Wire Arc Additive Manufacturing is to provide a comprehensive understanding of this innovative technology. This includes explaining how the process works, the materials used, and the applications where this technology can be beneficial.",
+    desc: [
+      "In a workshop, participants would explore Robotic Wire Arc Additive Manufacturing (WAAM) from design to manufacturing.",
+      "They would learn about the process, including using wire feedstock to create parts layer by layer.",
+      "The workshop would also cover the benefits of WAAM, such as cost-effectiveness and efficiency in producing moderate-sized components.",
+      "Participants would learn to maximize WAAM's potential, including its integration with other systems for in-process problem detection and repair.",
+    ],
+    venue: "TBA",
+    req: ["Nil"],
+    maxPeople: null,
+    day: "16th March",
+    rules: ["Nil"],
+    coord: ["TBA"],
+    ph: "TBA",
   },
 ];
 
