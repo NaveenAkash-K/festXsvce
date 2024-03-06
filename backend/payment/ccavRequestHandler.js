bc2af2c28c4c9abf3b062147dc7c70c34ffb76d1
Naveen Akash <naveen.akash0904@gmail.com>
2024-03-06 17:45:27 +0530

    Payment

--- backend/payment/ccavRequestHandler.js
+++ backend/payment/ccavRequestHandler.js
@@ -13,6 +13,9 @@ var uuidv4 = require("uuid").v4;
 
 exports.postReq = function (request, response) {
   // 3342525
+  var parsedData;
+  var ordId;
+  var customerId;
   var body = "",
     workingKey = "BC9E44F0087D201330A6DE18039F21E0", //Put in the 32-Bit key shared by CCAvenues.
     accessCode = "AVGY68LC16AH21YGHA", //Put in the Access Code shared by CCAvenues.
@@ -21,11 +24,29 @@ exports.postReq = function (request, response) {
 
   request.on("data", function (data) {
     body += data;
-    var ordId = "ORD_" + uuidv4();
-    var parsedData = qs.parse(body);
-    console.log(parsedData);
-    var customerId = Date.now() + "_" + parsedData.regNo;
+    ordId = "ORD_" + uuidv4();
+    parsedData = qs.parse(body);
+    customerId = Date.now() + "_" + parsedData.regNo;
 
+    body += `&merchant_id=3342525
+      &order_id=${ordId}
+      &currency=INR
+      &amount=1
+      &redirect_url=https://technoways-svce-backend.vercel.app/ccavResponseHandler
+      &cancel_url=https://technoways-svce-backend.vercel.app/ccavResponseHandler
+      &language=EN
+      &billing_country=India
+      &customer_identifier=${customerId}`;
+    encRequest = ccav.encrypt(body, workingKey);
+    formbody =
+      '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' +
+      encRequest +
+      '"><input type="hidden" name="access_code" id="access_code" value="' +
+      accessCode +
+      '"><script language="javascript">document.redirect.submit();</script></form>';
+  });
+
+  request.on("end", async function () {
     new User({
       username: parsedData.billing_name,
       customerId: customerId,
@@ -49,26 +70,6 @@ exports.postReq = function (request, response) {
       .catch((error) => {
         console.log(error);
       });
-
-    body += `&merchant_id=3342525
-      &order_id=${ordId}
-      &currency=INR
-      &amount=1
-      &redirect_url=https://technoways-svce-backend.vercel.app/ccavResponseHandler
-      &cancel_url=https://technoways-svce-backend.vercel.app/ccavResponseHandler
-      &language=EN
-      &billing_country=India
-      &customer_identifier=${customerId}`;
-    encRequest = ccav.encrypt(body, workingKey);
-    formbody =
-      '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' +
-      encRequest +
-      '"><input type="hidden" name="access_code" id="access_code" value="' +
-      accessCode +
-      '"><script language="javascript">document.redirect.submit();</script></form>';
-  });
-
-  request.on("end", async function () {
     response.writeHeader(200, { "Content-Type": "text/html" });
     response.write(formbody);
     response.end();
