7011ff72ff0124889d3acf34e53bf5b7149820d1
Naveen Akash <naveen.akash0904@gmail.com>
2024-03-06 15:12:58 +0530

    Payment

--- backend/payment/ccavRequestHandler.js
+++ backend/payment/ccavRequestHandler.js
@@ -19,14 +19,14 @@ exports.postReq = function (request, response) {
     encRequest = "",
     formbody = "";
 
-  request.on("data", function (data) {
+  request.on("data", async function (data) {
     body += data;
     var ordId = "ORD_" + uuidv4();
     var parsedData = qs.parse(body);
     console.log(parsedData);
     var customerId = Date.now() + "_" + parsedData.regNo;
 
-    const user = new User({
+    const user = await new User({
       username: parsedData.billing_name,
       customerId: customerId,
       regNo: parsedData.regNo,
--- backend/payment/ccavResponseHandler.js
+++ backend/payment/ccavResponseHandler.js
@@ -97,86 +97,88 @@ exports.postRes = function (request, response) {
     `);
       response.end();
       return;
+    } else {
+      console.log("Response");
+      console.log(parsedData);
+
+      console.log(
+        await User.updateOne(
+          { email: parsedData.billing_email },
+          { $set: { paid: true } }
+        )
+      );
+
+      // console.log(user);
+
+      response.write(`
+        <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Payment Successful</title>
+          <style>
+              body {
+                  font-family: 'Arial', sans-serif;
+                  background-color: #f4f4f4;
+                  margin: 0;
+                  padding: 0;
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                  height: 100vh;
+              }
+      
+              .container {
+                  text-align: center;
+                  padding: 20px;
+                  border-radius: 10px;
+                  background-color: #fff;
+                  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                  max-width: 400px;
+                  width: 100%;
+              }
+      
+              h1 {
+                  color: #4CAF50;
+              }
+      
+              p {
+                  color: #333;
+              }
+      
+              .success-icon {
+                  color: #4CAF50;
+                  font-size: 48px;
+                  margin-bottom: 10px;
+              }
+      
+              .btn {
+                  display: inline-block;
+                  padding: 10px 20px;
+                  background-color: #4CAF50;
+                  color: #fff;
+                  text-decoration: none;
+                  border-radius: 5px;
+                  transition: background-color 0.3s;
+              }
+      
+              .btn:hover {
+                  background-color: #45a049;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <div class="success-icon">&#10004;</div>
+              <h1>Payment Successful</h1>
+              <p>Your payment was successfully processed. The QR code for your ticket has been sent to ${parsedData.billing_email}.</p>
+              <a href="#" class="btn">Continue</a>
+          </div>
+      </body>
+      </html>
+      `);
+      response.end();
     }
-
-    console.log("Response");
-    // console.log(parsedData.billing_name);
-
-    User.updateOne(
-      { email: parsedData.billing_email },
-      { $set: { paid: true } }
-    );
-
-    // console.log(user);
-
-    response.write(`
-      <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Payment Successful</title>
-        <style>
-            body {
-                font-family: 'Arial', sans-serif;
-                background-color: #f4f4f4;
-                margin: 0;
-                padding: 0;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                height: 100vh;
-            }
-    
-            .container {
-                text-align: center;
-                padding: 20px;
-                border-radius: 10px;
-                background-color: #fff;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                max-width: 400px;
-                width: 100%;
-            }
-    
-            h1 {
-                color: #4CAF50;
-            }
-    
-            p {
-                color: #333;
-            }
-    
-            .success-icon {
-                color: #4CAF50;
-                font-size: 48px;
-                margin-bottom: 10px;
-            }
-    
-            .btn {
-                display: inline-block;
-                padding: 10px 20px;
-                background-color: #4CAF50;
-                color: #fff;
-                text-decoration: none;
-                border-radius: 5px;
-                transition: background-color 0.3s;
-            }
-    
-            .btn:hover {
-                background-color: #45a049;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="success-icon">&#10004;</div>
-            <h1>Payment Successful</h1>
-            <p>Your payment was successfully processed. The QR code for your ticket has been sent to ${parsedData.billing_email}.</p>
-            <a href="#" class="btn">Continue</a>
-        </div>
-    </body>
-    </html>
-    `);
-    response.end();
   });
 };
