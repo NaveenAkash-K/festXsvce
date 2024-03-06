12560e60aafdb6065ecee99533e88e981b76fb73
Naveen Akash <naveen.akash0904@gmail.com>
2024-03-06 12:57:24 +0530

    Payment

--- backend/payment/ccavRequestHandler.js
+++ backend/payment/ccavRequestHandler.js
@@ -39,7 +39,9 @@ exports.postReq = function (request, response) {
     formbody =
       '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' +
       encRequest +
-      '"><input type="hidden" name="access_code" id="access_code" value="' +
+      '"><input name="regNo" value=' +
+      parsedData.regNo +
+      '/><input type="hidden" name="access_code" id="access_code" value="' +
       accessCode +
       '"><script language="javascript">document.redirect.submit();</script></form>';
   });
