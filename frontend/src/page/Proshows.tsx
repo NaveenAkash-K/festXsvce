7b5c3a5373b7439af1c29d9196bd4c7a165c3089
Naveen Akash <naveen.akash0904@gmail.com>
2024-03-11 20:14:16 +0530

    Proshows

--- frontend/src/page/Proshows.tsx
+++ frontend/src/page/Proshows.tsx
@@ -2,11 +2,17 @@ import React, { useEffect } from "react";
 import Header from "../components/Header";
 import styles from "../styles/page/proshows.module.css";
 import { NavLink } from "react-router-dom";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import ark1 from "../assets/images/ark1.jpg";
+import ark2 from "../assets/images/ark2.webp";
+
 const Proshows: React.FC = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
-    document.title = "Proshows | Technoways"
+    document.title = "Proshows | Technoways";
   }, []);
+
   return (
     <>
       <Header />
@@ -15,9 +21,41 @@ const Proshows: React.FC = () => {
         <h1 className={styles.title}>Proshows</h1>
         <br />
         <div className={styles.workContainer}>
-          <NavLink to={"/"} style={{ textDecoration: "none", color: "white" }}>
-            Coming Soon!!
-          </NavLink>
+          <h3>
+            The Pro-Show event of the Technoways is here to take you into the
+            world of music and excitement with dynamic energy and unforgettable
+            moments with your friends🎤🎶
+          </h3>
+          <br />
+          <h3>
+            Get mesmerized in the voice of the Don singer, the one and only
+            Adithya R K
+          </h3>
+          <br />
+          <center>
+            <div className={styles.details}>
+              <h3>Venue: MPH, SVCE</h3>
+              <br />
+              <h3>Time: 2PM onwards</h3>
+              <br />
+              <h3>Date: 16th March</h3>
+            </div>
+          </center>
+          <br />
+          <br />
+          <Carousel
+            showArrows={true}
+            infiniteLoop={true}
+            autoPlay={true}
+            dynamicHeight={true}
+          >
+            <div>
+              <img src={ark1} height={"100%"} alt="" />
+            </div>
+            <div>
+              <img src={ark2} height={"10%"} alt="" />
+            </div>
+          </Carousel>
         </div>
       </div>
     </>
