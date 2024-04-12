475ef4960dfe4ae53a5ba5acf713c179d2406bab
NaveenAkash-K <naveen.akash0904@gmail.com>
2024-04-12 22:39:16 +0530

    error

--- backend/index.js
+++ backend/index.js
@@ -47,126 +47,126 @@ app.get("/about", function (req, res) {
 });
 
 app.post("/ccavRequestHandler", async (req, res) => {
-  const qrCodeBuffer = await QRCode.toBuffer(
-    "Hellooouhbiu"
-  );
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL, // Your Gmail email address
-      pass: process.env.PASSWD, // Your Gmail password or an app-specific password
-      // user: "2021it0668@svce.ac.in", // Your Gmail email address
-      // pass: "09naveen", // Your Gmail password or an app-specific password
-    },
-  });
+  // const qrCodeBuffer = await QRCode.toBuffer(
+  //   "Hellooouhbiu"
+  // );
+  // const transporter = nodemailer.createTransport({
+  //   service: "gmail",
+  //   auth: {
+  //     user: process.env.EMAIL, // Your Gmail email address
+  //     pass: process.env.PASSWD, // Your Gmail password or an app-specific password
+  //     // user: "2021it0668@svce.ac.in", // Your Gmail email address
+  //     // pass: "09naveen", // Your Gmail password or an app-specific password
+  //   },
+  // });
 
-  const mailOptions = {
-    from: process.env.EMAIL,
-    // to: parsedData.billing_email.trim(),
-    to: "naveen.akash0904@gmail.com",
-    subject: "QR Code Email",
-    html: `
-    <!DOCTYPE html>
-    <html lang="en">
+  // const mailOptions = {
+  //   from: process.env.EMAIL,
+  //   // to: parsedData.billing_email.trim(),
+  //   to: "naveen.akash0904@gmail.com",
+  //   subject: "QR Code Email",
+  //   html: `
+  //   <!DOCTYPE html>
+  //   <html lang="en">
     
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Highways E-Ticket</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f4f4f4;
-                margin: 0;
-                padding: 0;
-                text-align: center;
-            }
+  //   <head>
+  //       <meta charset="UTF-8">
+  //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  //       <title>Highways E-Ticket</title>
+  //       <style>
+  //           body {
+  //               font-family: Arial, sans-serif;
+  //               background-color: #f4f4f4;
+  //               margin: 0;
+  //               padding: 0;
+  //               text-align: center;
+  //           }
     
-            .ticket-container {
-                max-width: 600px;
-                margin: 50px auto;
-                background-color: #fff;
-                padding: 20px;
-                border-radius: 8px;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            }
+  //           .ticket-container {
+  //               max-width: 600px;
+  //               margin: 50px auto;
+  //               background-color: #fff;
+  //               padding: 20px;
+  //               border-radius: 8px;
+  //               box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  //           }
     
-            .ticket-header {
-                padding: 10px;
-                border-radius: 8px 8px 0 0;
-            }
+  //           .ticket-header {
+  //               padding: 10px;
+  //               border-radius: 8px 8px 0 0;
+  //           }
     
-            .ticket-content {
-                padding: 20px;
-            }
+  //           .ticket-content {
+  //               padding: 20px;
+  //           }
     
-            .qr-code {
-                margin-top: 20px;
-            }
+  //           .qr-code {
+  //               margin-top: 20px;
+  //           }
     
-            .footer-text {
-                margin-top: 20px;
-                color: #888;
-            }
+  //           .footer-text {
+  //               margin-top: 20px;
+  //               color: #888;
+  //           }
     
-            /* Different colors for ticket types */
-            .ticket-header.Day1 {
-                background-color: #3498db;
-                color: #fff;
-            }
+  //           /* Different colors for ticket types */
+  //           .ticket-header.Day1 {
+  //               background-color: #3498db;
+  //               color: #fff;
+  //           }
     
-            .ticket-header.Day2 {
-                background-color: #27ae60;
-                color: #fff;
-            }
+  //           .ticket-header.Day2 {
+  //               background-color: #27ae60;
+  //               color: #fff;
+  //           }
     
-            .ticket-header.Combo {
-                background-color: #ffd700;
-                color: #black;
-            }
-        </style>
-    </head>
+  //           .ticket-header.Combo {
+  //               background-color: #ffd700;
+  //               color: #black;
+  //           }
+  //       </style>
+  //   </head>
     
-    <body>
-        <div class="ticket-container">
-            <div class="ticket-header Day1">
-                <center>
-                    <h2>Highways E-Ticket</h2>
-                </center>
-                <center>
-                    <h1>Day1 Ticket</h1>
-                </center>
-            </div>
-            <div class="ticket-content">
-                <p>Hi [user.username]</p>
-                <p>We are delighted to have you registered for Highways! Here are your e-ticket details for the exciting event taking place on the __ and __ dates. We look forward to welcoming you!</p>
-                <div class="qr-code">
-                    <p>Hello valued participant,</p>
-                    <p>Your personalized QR code awaits you below:</p>
-                    <!-- Add QR Code here using the provided code snippet -->
-                    <img src="cid:qrcode@unique" width=100% alt="QR Code" />
-                </div>
-                <p class="footer-text">Kindly allow a moment for the QR code to be generated. Thank you for your patience.</p>
-                <p class="footer-text">Please present this QR code at the entrance during the event.</p>
-            </div>
-        </div>
-    </body>
+  //   <body>
+  //       <div class="ticket-container">
+  //           <div class="ticket-header Day1">
+  //               <center>
+  //                   <h2>Highways E-Ticket</h2>
+  //               </center>
+  //               <center>
+  //                   <h1>Day1 Ticket</h1>
+  //               </center>
+  //           </div>
+  //           <div class="ticket-content">
+  //               <p>Hi [user.username]</p>
+  //               <p>We are delighted to have you registered for Highways! Here are your e-ticket details for the exciting event taking place on the __ and __ dates. We look forward to welcoming you!</p>
+  //               <div class="qr-code">
+  //                   <p>Hello valued participant,</p>
+  //                   <p>Your personalized QR code awaits you below:</p>
+  //                   <!-- Add QR Code here using the provided code snippet -->
+  //                   <img src="cid:qrcode@unique" width=100% alt="QR Code" />
+  //               </div>
+  //               <p class="footer-text">Kindly allow a moment for the QR code to be generated. Thank you for your patience.</p>
+  //               <p class="footer-text">Please present this QR code at the entrance during the event.</p>
+  //           </div>
+  //       </div>
+  //   </body>
     
-    </html>
+  //   </html>
     
-  `,
-  attachments: [
-    {
-      filename: "qrcode.png",
-      content: qrCodeBuffer,
-      encoding: "base64",
-      cid: "qrcode@unique", // Content-ID for referencing the image in the HTML
-    },
-  ],
-  };
+  // `,
+  // attachments: [
+  //   {
+  //     filename: "qrcode.png",
+  //     content: qrCodeBuffer,
+  //     encoding: "base64",
+  //     cid: "qrcode@unique", // Content-ID for referencing the image in the HTML
+  //   },
+  // ],
+  // };
 
   // const info = await transporter.sendMail(mailOptions);
-  console.log("Email sent:", info);
+  // console.log("Email sent:", info);
 
   ccavReqHandler.postReq(req, res);
 });
